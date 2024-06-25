4e453aa453e7f8243f7d9dbf04a6a2555c6e06d8
Dylan Robinaud <dylan.rbnd@gmail.com>
2024-06-25 10:20:33 +0200

    Réalisation de la quête n°3

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 
+const { sayWelcome } = require("../../controllers/sayActions");
+const { browse } = require("../../controllers/programAction");
+
+router.get("/", sayWelcome);
+router.get("/programs", browse);
+
 /* ************************************************************************* */
 // Import And Use Routers Here
 /* ************************************************************************* */
